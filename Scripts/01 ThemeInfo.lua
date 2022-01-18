5aea9612b2a78643c4704336aa8a4b3fc558dab3
Inorizushi <inorizushi@gmail.com>
2022-01-18 17:05:13 -0600

    add re-write tag

--- Scripts/01 ThemeInfo.lua
+++ Scripts/01 ThemeInfo.lua
@@ -1,7 +1,7 @@
 -- theme identification:
 themeInfo = {
-ProductCode = "DDR XX2.0",
-Name = "DDR XX -starlight- twopointzero-dev",
+ProductCode = "DDR XX2.0-re",
+Name = "DDR XX -starlight- twopointzero-rewrite",
 Version = "V2.0.0",
 Date = "20211127",
 Author = "XX team, silverdragon designs, Inorizushi/Hooni, tertu"
