071b430b3c6eb3bc4635c72a3ec9952e197e3a97
Inorizushi <inorizushi@gmail.com>
2022-02-01 18:42:34 -0600

    clean fonts folder up further, add VideoRenderers to Graphic options, add another SortList button combo

--- Scripts/00 AInit.lua
+++ Scripts/00 AInit.lua
@@ -81,3 +81,72 @@ end;
 local function asdf()
 	return _G[obf('VG9FbnVtU2hvcnRTdHJpbmc=')](_G[obf('R0FNRVNUQVRF')][obf('R2V0Q29pbk1vZGU=')](_G[obf('R0FNRVNUQVRF')]));
 end;
+
+-- iterates over a numerically-indexed table (haystack) until a desired value (needle) is found
+-- if found, return the index (number) of the desired value within the table
+-- if not found, return nil
+function FindInTable(needle, haystack)
+	for i = 1, #haystack do
+		if needle == haystack[i] then
+			return i
+		end
+	end
+	return nil
+end
+
+function VideoRenderer()
+
+	-- opengl is a valid VideoRenderer for all platforms right now
+	-- so start by assuming it is the only choice.
+	-- If there is a method available to Lua to get available renderers
+	-- from the engine, I haven't found it yet.
+	local choices = { "opengl" }
+	local values  = { "opengl" }
+
+	-- Windows also has d3d as a VideoRenderer on SM 5.1, and SM 5.3
+	-- features a modern OpenGL based backend (glad) on all supported
+	-- platforms. The convention(?) there is to list both available
+	-- backends in Preferences.ini, but only use the first
+	local architecture = HOOKS:GetArchName():lower()
+	if _VERSION == "Lua 5.3" then
+		table.insert(choices, "glad")
+		values = { "opengl,glad", "glad,opengl" }
+	elseif architecture:match("windows") then
+		table.insert(choices, "d3d")
+		values = { "opengl,d3d", "d3d,opengl" }
+	end
+
+	return {
+		Name = _VERSION == "Lua 5.3" and "VideoRendererSM5.3" or "VideoRenderer",
+		Choices = choices,
+		LayoutType = "ShowAllInRow",
+		SelectType = "SelectOne",
+		OneChoiceForAllPlayers = true,
+		ExportOnChange = false,
+		LoadSelections = function(self, list, pn)
+			local pref = PREFSMAN:GetPreference("VideoRenderers")
+
+			-- Multiple comma-delimited VideoRenderers may be listed, but
+			-- we only want the first because that's the one actually in use.
+			-- Split the string on commas, get the first match found, and
+			-- immediately break from the loop.
+			for renderer in pref:gmatch("(%w+),?") do
+				pref = renderer
+				break
+			end
+
+			if not pref then return end
+
+			local i = FindInTable(pref, self.Choices) or 1
+			list[i] = true
+		end,
+		SaveSelections = function(self, list, pn)
+			for i=1, #list do
+				if list[i] then
+					PREFSMAN:SetPreference("VideoRenderers", values[i])
+					break
+				end
+			end
+		end,
+	}
+end
