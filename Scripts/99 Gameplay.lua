28e8159886cbac51d8f6e5515e8ad0984f7ad2dc
sillybear <jindev2k7@yahoo.com>
2022-04-08 23:57:15 +0800

    Update BeginOutDelay() function
    fix to gameplay song end delay before shutter close

--- Scripts/99 Gameplay.lua
+++ Scripts/99 Gameplay.lua
@@ -124,14 +124,18 @@ function BeginOutDelay()
 	local bpm = round(td:GetBPMAtBeat(GetSong():GetLastBeat()),3)
 	local m = 1
 	
-	if bpm < 60 then
+	if bpm >= 60 and bpm < 120 then
+		m = 0.75
+	elseif bpm < 60 then
 		m = 0.5
-	elseif bpm >= 240 then
-		m = 2
-	elseif bpm >= 480 then
-		m = 4
-	elseif bpm >= 960 then
-		m = 8
+	else
+		--- 240 bpm and above
+		for i=1, 3 do
+			if bpm >= 240*(3-(i-1)) then
+				m = 2*(3-(i-1))
+				break
+			end
+		end
 	end
 	
 	local timeSigs = split('=', td:GetTimeSignatures()[1])
