1c3b511c5e96bca16c03c0e930837724c5a001af
Inorizushi <inorizushi@gmail.com>
2023-08-26 06:34:35 -0500

    Fix ScrollBar

--- Graphics/ScrollBar middle/default.lua
+++ Graphics/ScrollBar middle/default.lua
@@ -1,14 +1,16 @@
-return Def.Sprite{
-    Texture="A",
-    InitCommand=function(s) s:zoomtoheight(1)
-        if GAMESTATE:IsCourseMode() == false then
-            if ThemePrefs.Get("WheelType") == "A" or ThemePrefs.Get("WheelType") == "Wheel" then
-                s:visible(true)
-         else
+return Def.ActorFrame{
+    Def.Sprite{
+        Texture="A",
+        InitCommand=function(s) s:zoomtoheight(1)
+           if GAMESTATE:IsCourseMode() == false then
+                if ThemePrefs.Get("WheelType") == "A" or ThemePrefs.Get("WheelType") == "Wheel" then
+                    s:visible(true)
+            else
+                    s:visible(false)
+                end
+            else
                 s:visible(false)
             end
-        else
-            s:visible(false)
-        end
-    end,
+        end,
+    }
 }
