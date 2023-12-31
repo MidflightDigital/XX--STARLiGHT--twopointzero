ec8f0d6fed71528267c434698aa2dcc84c03c477
Inorizushi <inorizushi@gmail.com>
2023-12-31 00:14:31 -0600

    Convert Eval elements to text actors

--- BGAnimations/ScreenEvaluationNormal decorations/frame.lua
+++ BGAnimations/ScreenEvaluationNormal decorations/frame.lua
@@ -49,28 +49,193 @@ local t = Def.ActorFrame{
     Def.Sprite{
         Texture="_box",
     };
-    Def.Sprite{
-        Texture="box header 1x3.png",
-        InitCommand=function(s) s:pause():y(-144):setstate(paneState) end,
-        CodeMessageCommand=function(self,params)
-			if params.PlayerNumber==controller then
-				self:setstate(paneState)
+	Def.ActorFrame{
+		Def.Sprite{
+			Name="Left",
+			Texture="header arrows 1x3.png",
+			InitCommand=function(s)
+				s:pause():rotationz(180):xy(-212,-144):setstate(paneState)
+				if paneState == 0 then
+					s:visible(false)
+				else
+					s:visible(true)
+				end
+			end,
+			CodeMessageCommand=function(self,params)
+				if params.PlayerNumber==controller then
+					self:setstate(paneState)
+					if paneState == 0 then
+						self:visible(false)
+					else
+						self:visible(true)
+					end
+				end;
 			end;
-        end;
-	};
-	Def.Sprite{
-        Texture="bottom 1x3.png",
-        InitCommand=function(s) s:pause():y(200):setstate(paneState) end,
-        CodeMessageCommand=function(self,params)
-			if params.PlayerNumber==controller then
-				self:setstate(paneState)
+		};
+		Def.Sprite{
+			Name="Right",
+			Texture="header arrows 1x3.png",
+			InitCommand=function(s)
+				s:pause():xy(212,-144):setstate(paneState)
+				if paneState == 2 then
+					s:visible(false)
+				else
+					s:visible(true)
+				end
+			end,
+			CodeMessageCommand=function(self,params)
+				if params.PlayerNumber==controller then
+					self:setstate(paneState)
+					if paneState == 2 then
+						self:visible(false)
+					else
+						self:visible(true)
+					end
+				end;
 			end;
-        end;
+		};
 	};
-	Def.Sprite{
-        Texture="instruct.png",
-        InitCommand=function(s) s:y(220) end,
-    };
+	Def.BitmapText{
+		Font="_avenirnext lt pro bold/glow/24 eval header",
+		InitCommand=function(s) s:y(-150)
+			:settext(THEME:GetString("ScreenEvaluation","Box Header"..paneState))
+			if paneState == 0 then
+				s:DiffuseAndStroke(color("#dff0ff"),color("#00baff"))
+			elseif paneState == 1 then
+				s:DiffuseAndStroke(color("#dfffe4"),color("#00ff60"))
+			elseif paneState == 2 then
+				s:DiffuseAndStroke(color("#ffdffc"),color("#ff00d2"))
+			end
+		end,
+		CodeMessageCommand=function(self,params)
+			if params.PlayerNumber==controller then
+				self:settext(THEME:GetString("ScreenEvaluation","Box Header"..paneState))
+				if paneState == 0 then
+					self:DiffuseAndStroke(color("#dff0ff"),color("#00baff"))
+				elseif paneState == 1 then
+					self:DiffuseAndStroke(color("#dfffe4"),color("#00ff60"))
+				elseif paneState == 2 then
+					self:DiffuseAndStroke(color("#ffdffc"),color("#ff00d2"))
+				end
+			end;
+		end;
+	};
+	Def.ActorFrame{
+		--Welcome to ActorFrame hell featuring Sunny.
+		InitCommand=function(s) s:y(188) end,
+		Def.ActorFrame{
+			Name="Results",
+			InitCommand=function(s) s:x(-253) end,
+			Def.BitmapText{
+				Font="_avenirnext lt pro bold/16px",
+				Text=THEME:GetString("ScreenEvaluation","Bottom0"),
+				InitCommand=function(s) s:strokecolor(Color.Black):maxwidth(300) end,
+			},
+		};
+		Def.ActorFrame{
+			Name="Rival",
+			Def.BitmapText{
+				Font="_avenirnext lt pro bold/16px",
+				Text=THEME:GetString("ScreenEvaluation","Bottom1"),
+				InitCommand=function(s) s:strokecolor(Color.Black):maxwidth(300) end,
+			},
+		};
+		Def.ActorFrame{
+			Name="Calories",
+			InitCommand=function(s) s:x(253) end,
+			Def.BitmapText{
+				Font="_avenirnext lt pro bold/16px",
+				Text=THEME:GetString("ScreenEvaluation","Bottom2"),
+				InitCommand=function(s) s:strokecolor(Color.Black):maxwidth(300) end,
+			},
+		};
+		Def.ActorFrame{
+			InitCommand=function(s)
+				s:y(20)
+				if paneState == 0 then
+					s:x(-253):zoomx(1)
+				elseif paneState == 1 then
+					s:x(0):zoomx(1.6)
+				elseif paneState == 2 then
+					s:x(253):zoomx(1.2)
+				else
+					s:x(0):zoomx(1.6)
+				end
+			end,
+			CodeMessageCommand=function(s,params)
+				if params.PlayerNumber==controller then
+					if paneState == 0 then
+						s:x(-253):zoomx(1)
+					elseif paneState == 1 then
+						s:x(0):zoomx(1.6)
+					elseif paneState == 2 then
+						s:x(253):zoomx(1.2)
+					else
+						s:x(0):zoomx(1.6)
+					end
+				end;
+			end;
+			Def.Sprite{
+				Texture="bottom bar glow.png",
+				InitCommand=function(s)
+					if paneState == 0 then
+						s:diffuse(color("#00baff"))
+					elseif paneState == 1 then
+						s:diffuse(color("#00ff60"))
+					elseif paneState == 2 then
+						s:diffuse(color("#ff00d2"))
+					else
+						s:diffuse(Color.White)
+					end
+				end,
+				CodeMessageCommand=function(s,params)
+					if params.PlayerNumber==controller then
+						if paneState == 0 then
+							s:diffuse(color("#00baff"))
+						elseif paneState == 1 then
+							s:diffuse(color("#00ff60"))
+						elseif paneState == 2 then
+							s:diffuse(color("#ff00d2"))
+						else
+							s:diffuse(Color.White)
+						end
+					end;
+				end;
+			},
+			Def.Sprite{
+				Texture="bottom bar.png",
+				InitCommand=function(s)
+					if paneState == 0 then
+						s:diffuse(color("#dff0ff"))
+					elseif paneState == 1 then
+						s:diffuse(color("#dfffe4"))
+					elseif paneState == 2 then
+						s:diffuse(color("#ffdffc"))
+					else
+						s:diffuse(Color.White)
+					end
+				end,
+				CodeMessageCommand=function(s,params)
+					if params.PlayerNumber==controller then
+						if paneState == 0 then
+							s:diffuse(color("#dff0ff"))
+						elseif paneState == 1 then
+							s:diffuse(color("#dfffe4"))
+						elseif paneState == 2 then
+							s:diffuse(color("#ffdffc"))
+						else
+							s:diffuse(Color.White)
+						end
+					end;
+				end;
+			},
+		}
+	};
+	Def.BitmapText{
+		Font="_avenirnext lt pro bold/16px",
+		Text=THEME:GetString("ScreenEvaluation","BottomInstruct"),
+		InitCommand=function(s) s:y(210) end,
+	},
     Def.ActorFrame{
 		InitCommand=function(s)
 			if paneState == 0 then
